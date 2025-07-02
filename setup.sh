965c5c43677dff7da996648c4dab9bdbadb35d79
JLEESH <jimmyleesh@yahoo.co.kr>
2025-07-03 01:23:00 +0900

    second dummy commit.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 # This script sets up the environment for the project.
 # Install required Python packages
-pip install -r requirements.txt
+#pip install -r requirements.txt
+pip install datasets transformers accelerate wandb
 
 # Initialize Weights & Biases
 # wandb init --project "qmodel-ft" --entity "your_wandb_entity"
